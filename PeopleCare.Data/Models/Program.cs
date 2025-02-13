a9ff512176769cf5d95b4ac714c1c95e652bb701
Meg <Meg.Gravatt@IntelliTect.com>
2025-02-13 08:12:36 -0800

    Fix model errors

--- PeopleCare.Data/Models/Program.cs
+++ PeopleCare.Data/Models/Program.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PeopleCare.Data.Models;
+﻿namespace PeopleCare.Data.Models;
 public class Program: TenantedBase
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string ProgramId { get; set; } = null!;
-
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
 
-    public ICollection<FundingSource> FundingSources { get; set; } = null!;
+    [ManyToMany($"{nameof(FundingSource)}s", FarNavigationProperty = nameof(ProgramFundingSource.FundingSource))]
+    public ICollection<ProgramFundingSource> ProgramFundingSources { get; set; } = [];
 
-    public ICollection<Activity> Activities { get; set; } = null!;
+    public ICollection<Activity> Activities { get; set; } = [];
 }
